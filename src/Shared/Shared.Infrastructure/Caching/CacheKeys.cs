d3176c2bb0cadaa0eb335f947f47aaa7bfd1aa9d
zhilong <yzl_200@sina.com>
2021-11-09 14:56:23 +0800

    add cachekey

---
+++ src/Shared/Shared.Infrastructure/Caching/CacheKeys.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Infrastructure.Entities;
+using Shared.Infrastructure.Utilities;
+
+namespace Shared.Infrastructure.Caching
+{
+    public static class CacheKeys
+    {
+        public static class Common
+        {
+            public static string GetEntityByIdCacheKey<TEntityId, TEntity>(TEntityId id)
+                where TEntity : class, IEntity<TEntityId>
+            {
+                return $"GetEntity-{typeof(TEntity).GetGenericTypeName()}-{id}";
+            }
+        }
+    }
+}
